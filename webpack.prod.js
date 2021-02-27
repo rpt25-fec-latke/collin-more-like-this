a69f070ad0a4b86d27573193abc1c3a60a920abb
Collin <collinmcmahon2014@yahoo.com>
2021-02-26 16:32:42 -1000

    taking out s3 webpack plugin to run on instance

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const S3Plugin = require('webpack-s3-plugin');
+// const S3Plugin = require('webpack-s3-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -24,17 +24,17 @@ module.exports = merge(common, {
     ],
   },
   plugins: [
-    new S3Plugin({
-      s3Options: {
-        exclude: /.*\.(html|txt)/,
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        region: 'us-east-2',
-      },
-      s3UploadOptions: {
-        Bucket: 'steam-bundles',
-      },
-    }),
+    // new S3Plugin({
+    //   s3Options: {
+    //     exclude: /.*\.(html|txt)/,
+    //     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    //     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    //     region: 'us-east-2',
+    //   },
+    //   s3UploadOptions: {
+    //     Bucket: 'steam-bundles',
+    //   },
+    // }),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
     new UglifyJSPlugin(),
   ],
